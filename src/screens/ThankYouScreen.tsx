9e7b49b980d7717149526a50918a91e04dc54503
Bob <rbsavary88@gmail.com>
2023-07-10 15:39:35 -0400

    Add ThankYouScreen

--- src/screens/ThankYouScreen.tsx
+++ src/screens/ThankYouScreen.tsx
@@ -1,12 +1,21 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Pressable, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 type Props = {}
 
 const ThankYouScreen = (props: Props) => {
     return (
-        <SafeAreaView>
-            <Text>ThankYouScreen</Text>
+        <SafeAreaView className="mx-auto my-auto ">
+            <Text className="text-4xl">Thank You!</Text>
+            <Text className="text-lg font-bold">Your test results have been reported.</Text>
+            <View className="border-2 border-black w-[342] h-[339] mt-[100] "></View>
+            <Text className="text-lg font-bold">Join our community and save time on your next reporting by making an account today!</Text>
+            <TouchableOpacity className="border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg ">
+                <Text className="text-lg font-bold">Create Account</Text>
+            </TouchableOpacity>
+            <Pressable>
+                <Text className="text-lg font-bold">Back</Text>
+            </Pressable>
         </SafeAreaView>
     )
 }
