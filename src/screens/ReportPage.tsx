a11c4de1397ce73e64f12a11e0cac7c3a2fde5cb
Bob <rbsavary88@gmail.com>
2023-07-10 14:46:26 -0400

    Add form with button to ReportPage

--- src/screens/ReportPage.tsx
+++ src/screens/ReportPage.tsx
@@ -1,11 +1,14 @@
-import { View, Text, SafeAreaView, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, } from 'react-native'
+import React, { useState } from 'react'
+import CheckBox from '@react-native-community/checkbox';
 
 type Props = {}
 
 const ReportPage = (props: Props) => {
+    const [toggleCheckBox, setToggleCheckBox] = useState(false)
+
     return (
-        <SafeAreaView className="mx-auto flex justify-center">
+        <SafeAreaView className="mx-auto my-auto flex justify-center">
             <View className="h-[110] mt-[46]">
                 <Text className="text-lg font-bold">Report COVID-19 Test Result</Text>
             </View>
@@ -13,10 +16,32 @@ const ReportPage = (props: Props) => {
                 <Text className="text-lg font-bold">What were the results of your test?</Text>
                 <View></View>
                 <View></View>
-                <Text>Zip Code*</Text>
-                <TextInput></TextInput>
+                <Text className="font-bold">Zip Code*</Text>
+                <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                <Text className="font-bold">Age*</Text>
+                <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                <Text className="font-bold">Gender</Text>
+                <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                <Text className="font-bold">Race</Text>
+                <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                <Text className="font-bold">Ethnicity</Text>
+                <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                <View>
+                    {/* <CheckBox
+                        disabled={false}
+                        value={toggleCheckBox}
+                        onValueChange={(newValue) => setToggleCheckBox(newValue)}
+
+                    /> */}
+                    <Text className="font-bold">I agree to share my results with the CDC</Text>
+                </View>
             </View>
-        </SafeAreaView>
+            <View>
+                <TouchableOpacity className="border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg bg-[#B4B4B4]">
+                    <Text className="text-lg font-bold">Report Without Account</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView >
     )
 }
 
