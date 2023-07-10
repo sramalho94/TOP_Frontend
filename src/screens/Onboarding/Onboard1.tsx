bc763ebb8b5c18d15403f66c67e67250c92980f3
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-10 16:50:19 -0500

    made styling and elements on onboard page 1

--- src/screens/Onboarding/Onboard1.tsx
+++ src/screens/Onboarding/Onboard1.tsx
@@ -5,6 +5,7 @@ import {
     Text,
     TouchableOpacity,
     Image,
+    ScrollView
 } from 'react-native';
 import NoImage from '../../../assets/nopicture.png'
 
@@ -13,11 +14,38 @@ const Onboard1 = () => {
 
     return (
         <>
-            <SafeAreaView>
-                <View className="">
-                    <Image source={NoImage}></Image>
-                    <Text>What the app does and the impact of data</Text>
-                </View>
+            <SafeAreaView className="h-screen w-screen">
+                <ScrollView>
+                    <View className="flex-1">
+
+                        {/* image and text */}
+                        <View className="m-5">
+                            <Image className="w-full" source={NoImage}></Image>
+                            <Text className="mx-auto py-5">What the app does and the impact of data</Text>
+                        </View>
+
+                        {/* container for dots and buttons */}
+                        <View className="flex-1 flex-end justify-self-end">
+
+                            {/* progress dots */}
+                            <View className="flex-row mx-auto mt-72">
+                                <View className="rounded-lg bg-black w-3 h-3 m-1"></View>
+                                <View className="rounded-lg bg-gray-300 w-3 h-3 m-1"></View>
+                                <View className="rounded-lg bg-gray-300 w-3 h-3 m-1"></View>
+                            </View>
+
+                            {/* buttons */}
+                            <View className="m-5">
+                                <TouchableOpacity className="rounded border justify-items-center">
+                                    <Text className="text-center py-3">Continue</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity className="justify-items-center">
+                                    <Text className="text-center py-3">Skip</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
             </SafeAreaView>
         </>
     )
