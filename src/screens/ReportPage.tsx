dd6345867a1abe46920394685bc420e7edb0ea29
Bob <rbsavary88@gmail.com>
2023-07-10 13:47:57 -0400

    Add text to ReportPage

--- src/screens/ReportPage.tsx
+++ src/screens/ReportPage.tsx
@@ -5,9 +5,12 @@ type Props = {}
 
 const ReportPage = (props: Props) => {
     return (
-        <SafeAreaView className="mx-auto">
+        <SafeAreaView className="mx-auto flex justify-center">
             <View className="h-[110] mt-[46]">
                 <Text className="text-lg font-bold">Report COVID-19 Test Result</Text>
+            </View>
+            <View>
+                <Text className="text-lg font-bold">What were the results of your test?</Text>
             </View>
         </SafeAreaView>
     )
