524272ffc56e7cb7c084d4f17374b689c19f0fb8
Bob <rbsavary88@gmail.com>
2023-07-11 15:53:09 -0400

    Add styling to form in ReportPage

--- src/screens/ReportPage.tsx
+++ src/screens/ReportPage.tsx
@@ -1,22 +1,28 @@
 import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Pressable, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React from 'react'
 import CheckBox from '@react-native-community/checkbox';
 import Icon from 'react-native-vector-icons/AntDesign'
+import DropDownPicker from 'react-native-dropdown-picker';
+
+// function App() {
+//     const [open, setOpen] = useState(false);
+//     const [value, setValue] = useState(null);
+//     const [items, setItems] = useState([
+//         { label: 'Apple', value: 'apple' },
+//         { label: 'Banana', value: 'banana' }
+//     ]);
 type Props = {}
-
-
 
 const ReportPage = (props: Props) => {
 
-
     return (
-        <SafeAreaView className="mx-auto my-auto">
+        <SafeAreaView className="">
             <ScrollView>
-                <View className="h-[110] mt-[46] ">
+                <View className="h-[110] border-b-4 border-slate-200 flex-row my-6">
                     <Icon name="arrowleft" size={30} color="#000" />
-                    <Text className="text-lg font-bold mx-auto">Report COVID-19 Test Result</Text>
+                    <Text className="text-lg font-bold mx-auto mt-2 flex justify-center">Report COVID-19 Test Result</Text>
                 </View>
-                <View>
+                <View className="mx-auto my-auto">
                     <Text className="text-lg font-bold mx-auto">What were the results of your test?</Text>
                     <View className="justify-center space-x-4 flex-row my-9">
                         <Pressable className="border-4 border-black flex items-center w-[125] h-[125] rounded-full justify-center">
@@ -26,33 +32,58 @@ const ReportPage = (props: Props) => {
                             <Text>Positive</Text>
                         </Pressable>
                     </View>
-                    <Text className="font-bold">Zip Code*</Text>
-                    <TextInput className="border-2 border-black rounded-lg"></TextInput>
-                    <Text className="font-bold">Age*</Text>
-                    <TextInput className="border-2 border-black rounded-lg"></TextInput>
-                    <Text className="font-bold">Gender</Text>
-                    <TextInput className="border-2 border-black rounded-lg"></TextInput>
-                    <Text className="font-bold">Race</Text>
-                    <TextInput className="border-2 border-black rounded-lg"></TextInput>
-                    <Text className="font-bold">Ethnicity</Text>
-                    <TextInput className="border-2 border-black rounded-lg"></TextInput>
-                    <View>
-                        {/* <CheckBox
-                        disabled={false}
-                        value={toggleCheckBox}
-                        onValueChange={(newValue) => setToggleCheckBox(newValue)}
+                    <View className='w-[342]'>
+                        <Text className="font-bold my-2">Zip Code*</Text>
+                        <TextInput className="border-2 border-black rounded-lg"></TextInput>
+                        <Text className="font-bold my-2">Age*</Text>
+                        <TextInput className="border-2 border-black rounded-lg"></TextInput>
 
-                    /> */}
-                        <Text className="font-bold">I agree to share my results with the CDC</Text>
+                        <Text className="font-bold my-2">Gender</Text>
+                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
+                        // open={open}
+                        // value={value}
+                        // items={items}
+                        // setOpen={setOpen}
+                        // setValue={setValue}
+                        // setItems={setItems}
+
+                        />
+                        <Text className="font-bold my-2">Race</Text>
+                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
+                        // open={open}
+                        // value={value}
+                        // items={items}
+                        // setOpen={setOpen}
+                        // setValue={setValue}
+                        // setItems={setItems}
+
+                        />
+                        <Text className="font-bold my-2">Ethnicity</Text>
+                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
+                        // open={open}
+                        // value={value}
+                        // items={items}
+                        // setOpen={setOpen}
+                        // setValue={setValue}
+                        // setItems={setItems}
+
+                        />
+                    </View>
+                    <View className='flex-row justify-center my-6'>
+                        <CheckBox />
+                        <Text className="font-bold mt-1">I agree to share my results with the CDC</Text>
                     </View>
                 </View>
 
-                <TouchableOpacity className="border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg bg-[#B4B4B4]">
-                    <Text className="text-lg font-bold">Report Without Account</Text>
-                </TouchableOpacity>
+                <View className='border-t-4 border-b-4 border-slate-200 '>
+                    <TouchableOpacity className="border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg bg-[#B4B4B4] mt-6 mb-8 mx-auto">
+                        <Text className="text-lg font-bold">Report</Text>
+                    </TouchableOpacity>
+                </View>
             </ScrollView>
         </SafeAreaView >
     )
 }
+
 
 export default ReportPage
