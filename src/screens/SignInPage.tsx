e8cb34397b9bb56079406155bb0568a1bf10c5df
chenannchi <anchichenusa@gmail.com>
2023-07-11 09:52:36 +0800

    user/password now visible

--- src/screens/SignInPage.tsx
+++ src/screens/SignInPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, TextInput, Image } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, TextInput, Image, ScrollView } from 'react-native';View
 import NoImage from "../../assets/blankimage.png"
 
 
@@ -11,82 +11,85 @@ export default function SignInPage() {
 
   return (
     <SafeAreaView className="w-342 m-6">
-      <View className="">
-        <View className="flex flex-row justify-center align-middle">
-          <Image className="bg-green-500 w-342 h-349 m-4" source={NoImage}>
-          </Image>
-        </View>
+      <ScrollView>
+        <View className="">
+          <View className="flex flex-row justify-center align-middle">
+            <Image className="bg-green-500 w-342 h-349 m-4" source={NoImage}>
+            </Image>
+          </View>
 
-        <View className="m-6">
           <View className="m-6">
-            <Text className="absolute w-36 h-11 flex items-center justify-center text-black font-medium">
-              Username
-            </Text>
+            <View className="m-6 bg-red-500">
+              <Text className="w-36 h-11 flex items-center justify-center text-black font-medium">
+                Username
+              </Text>
 
-            <TextInput
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardType="email-address"
-              onChangeText={email => setForm({ ...form, email })}
-              placeholder="john@example.com"
-              placeholderTextColor="#6b7280"
-              className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
-              value={form.email}
-            />
-          </View>
+              <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                onChangeText={email => setForm({ ...form, email })}
+                placeholder="john@example.com"
+                placeholderTextColor="#6b7280"
+                className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
+                value={form.email}
+              />
+            </View>
 
-          <View className="mb-6">
-            <Text className="absolute w-36 h-11 flex items-center justify-center text-gray-400 font-medium">
-              Password
-            </Text>
+            <View className="mb-6 bg-red-500">
+              <Text className="w-36 h-11 flex items-center justify-center text-black font-medium">
+                Password
+              </Text>
 
-            <TextInput
-              autoCorrect={false}
-              onChangeText={password => setForm({ ...form, password })}
-              placeholder="********"
-              placeholderTextColor="#6b7280"
-              className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
-              secureTextEntry={true}
-              value={form.password}
-            />
-          </View>
+              <TextInput
+                autoCorrect={false}
+                onChangeText={password => setForm({ ...form, password })}
+                placeholder="********"
+                placeholderTextColor="#6b7280"
+                className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
+                secureTextEntry={true}
+                value={form.password}
+              />
+            </View>
 
-          <View className="mb-10">
+            <View className="mb-10">
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}
+                className="flex items-center justify-center rounded-md bg-black border border-black py-2 px-4"
+              >
+                <Text className="text-white font-semibold text-base">
+                  Log In
+                </Text>
+              </TouchableOpacity>
+            </View>
+
             <TouchableOpacity
               onPress={() => {
-                // handle onPress
+                // handle link
               }}
-              className="flex items-center justify-center rounded-md bg-black border border-black py-2 px-4"
             >
-              <Text className="text-white font-semibold text-base">
-                Log In
+              <Text className="flex items-center justify-center rounded-md bg-black border border-black py-2 px-4">
+                Don't have an account?{' '}
+                <Text className="font-semibold text-white">Sign up</Text>
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => {
+                // handle link
+              }}
+            >
+              <Text className="text-sm font-normal text-gray-600 text-center">
+                Forgot Password?{' '}
+                <Text className="font-semibold underline">Reset Password</Text>
               </Text>
             </TouchableOpacity>
           </View>
-
-          <TouchableOpacity
-            onPress={() => {
-              // handle link
-            }}
-          >
-            <Text className="flex items-center justify-center rounded-md bg-black border border-black py-2 px-4">
-              Don't have an account?{' '}
-              <Text className="font-semibold text-white">Sign up</Text>
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={() => {
-              // handle link
-            }}
-          >
-            <Text className="text-sm font-normal text-gray-600 text-center">
-              Forgot Password?{' '}
-              <Text className="font-semibold underline">Reset Password</Text>
-            </Text>
-          </TouchableOpacity>
         </View>
-      </View>
+      </ScrollView>
+
     </SafeAreaView>
   );
 }
