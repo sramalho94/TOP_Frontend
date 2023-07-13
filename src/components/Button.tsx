939bcb2a3dc0fd278bfb35d3970fcde497ed51db
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-13 12:56:02 -0400

    button now takes border boolean prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,12 +5,15 @@ type Props = {
   onPress: () => void;
   text: string;
   bgColor: string;
+  border?: boolean;
 };
 
-const Button = ({onPress, text, bgColor}: Props) => {
+const Button = ({onPress, text, bgColor, border}: Props) => {
   return (
     <TouchableOpacity
-      className={`border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg mb-3 ${bgColor}`}
+      className={`${
+        border ? 'border-4 border-black' : ''
+      } flex justify-center items-center w-[342] h-[52] rounded-lg mb-3 ${bgColor}`}
       onPress={onPress}>
       <Text className="text-lg font-bold">{text}</Text>
     </TouchableOpacity>
