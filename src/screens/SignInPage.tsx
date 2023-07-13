224f6f936c697b5700ccd88debbc91a84b472fa0
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-13 12:40:31 -0400

    ts configuration to remove error with classNames

--- src/screens/SignInPage.tsx
+++ src/screens/SignInPage.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, TouchableOpacity, TextInput, Image, ScrollView } from 'react-native';
-import NoImage from "../../assets/blankimage.png"
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  ScrollView,
+} from 'react-native';
+import NoImage from '../../assets/blankimage.png';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-type Props = {}
+type Props = {};
 
 type FormState = {
   username: string;
@@ -18,10 +26,10 @@ const initialFormState: FormState = {
 };
 
 export default function SignInPage(props: Props) {
-  const [form, setForm] = useState<FormState>(initialFormState)
+  const [form, setForm] = useState<FormState>(initialFormState);
 
   const togglePasswordVisibility = () => {
-    setForm((prevState) => ({
+    setForm(prevState => ({
       ...prevState,
       showPassword: !prevState.showPassword,
     }));
@@ -32,8 +40,7 @@ export default function SignInPage(props: Props) {
       <ScrollView>
         <View className="">
           <View className="flex flex-row justify-center align-middle">
-            <Image className="w-342 h-349 m-4" source={NoImage}>
-            </Image>
+            <Image className="w-342 h-349 m-4" source={NoImage}></Image>
           </View>
 
           <View className="m-6">
@@ -46,7 +53,7 @@ export default function SignInPage(props: Props) {
                 autoCapitalize="none"
                 autoCorrect={false}
                 keyboardType="default"
-                onChangeText={username => setForm({ ...form, username })}
+                onChangeText={username => setForm({...form, username})}
                 placeholderTextColor="#6b7280"
                 className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
                 value={form.username}
@@ -54,14 +61,13 @@ export default function SignInPage(props: Props) {
             </View>
 
             <View className="mb-6">
-
               <Text className="w-36 h-8 flex items-center justify-center text-black font-medium">
                 Password
               </Text>
 
               <TextInput
                 autoCorrect={false}
-                onChangeText={password => setForm({ ...form, password })}
+                onChangeText={password => setForm({...form, password})}
                 placeholderTextColor="#6b7280"
                 className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
                 secureTextEntry={!form.showPassword}
@@ -69,11 +75,14 @@ export default function SignInPage(props: Props) {
               />
               <TouchableOpacity
                 onPress={togglePasswordVisibility}
-                className="absolute top-10 right-4"
-                >
-                <Icon className="text-gray-500" type="Ionicons" name={form.showPassword ? 'eye-off-outline' : 'eye-outline'} size={25} />
+                className="absolute top-10 right-4">
+                <Icon
+                  className="text-gray-500"
+                  type="Ionicons"
+                  name={form.showPassword ? 'eye-off-outline' : 'eye-outline'}
+                  size={25}
+                />
               </TouchableOpacity>
-
             </View>
 
             <View className="mb-5">
@@ -81,8 +90,7 @@ export default function SignInPage(props: Props) {
                 onPress={() => {
                   // handle onPress
                 }}
-                className="flex items-center justify-center rounded-md bg-white border border-black py-2 px-4"
-              >
+                className="flex items-center justify-center rounded-md bg-white border border-black py-2 px-4">
                 <Text className="text-black font-semibold text-base">
                   Log In
                 </Text>
@@ -94,8 +102,7 @@ export default function SignInPage(props: Props) {
                 onPress={() => {
                   // handle onPress
                 }}
-                className="flex items-center justify-center rounded-md bg-white border border-black py-2 px-4"
-              >
+                className="flex items-center justify-center rounded-md bg-white border border-black py-2 px-4">
                 <Text className="text-black font-semibold text-base">
                   Forgot Password
                 </Text>
@@ -105,8 +112,7 @@ export default function SignInPage(props: Props) {
             <TouchableOpacity
               onPress={() => {
                 // handle link
-              }}
-            >
+              }}>
               <Text className="text-sm font-normal text-black text-center">
                 Back
               </Text>
@@ -114,7 +120,6 @@ export default function SignInPage(props: Props) {
           </View>
         </View>
       </ScrollView>
-
     </SafeAreaView>
   );
 }
