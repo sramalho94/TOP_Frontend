45ea2a2b3ba2c0cf68b684eda5887d9e3f0c8ecd
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-13 16:16:25 -0500

    making dev branch for development purposes, added themes to tailwind config

--- App.tsx
+++ App.tsx
@@ -19,9 +19,9 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import Onboard1 from './src/screens/onboarding/Onboard1';
-import Onboard2 from './src/screens/onboarding/Onboard2';
-import Onboard3 from './src/screens/onboarding/Onboard3';
+import Onboard1 from './src/screens/Onboarding/Onboard1';
+import Onboard2 from './src/screens/Onboarding/Onboard2';
+import Onboard3 from './src/screens/Onboarding/Onboard3';
 
 
 function App(): JSX.Element {
