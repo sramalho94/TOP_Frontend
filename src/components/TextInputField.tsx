e4335eb1e2f957ee1ed5c95de530b138f0491716
Bob <rbsavary88@gmail.com>
2023-07-14 13:11:24 -0400

    Add comments to TextInputField.tsx

--- src/components/TextInputField.tsx
+++ src/components/TextInputField.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { View, Text, TextInput } from 'react-native';
 
+// Define the props interface for TextInputField component
 interface TextInputFieldProps {
+  // Label for the input field
   label: string,
+  // Current value of the input field
   value: string,
+  // Function to handle input changes
   onChange: (value: string) => void;
 }
 
+// Define the TextInputField component
 const TextInputField: React.FC<TextInputFieldProps> = ({ label, value, onChange }) => {
+  // Function to handle input changes
   const handleChange = (inputValue: string) => {
     onChange(inputValue);
   };
-
+  // Render the component
   return (
     <View>
       <Text className="font-bold my-2">{label}</Text>
@@ -25,4 +31,5 @@ const TextInputField: React.FC<TextInputFieldProps> = ({ label, value, onChange 
   );
 };
 
+// Export the TextInputField component
 export default TextInputField;
