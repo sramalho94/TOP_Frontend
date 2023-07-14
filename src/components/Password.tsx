509ad3aef3b8be6be94178685af844d01036d4cf
sornekian <sarvornek@gmail.com>
2023-07-13 20:14:56 -0700

    updates password component

---
+++ src/components/Password.tsx
@@ -0,0 +1,30 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+
+
+<View className="mb-6">
+  <Text className="w-36 h-8 flex items-center justify-center text-black font-medium">
+    Password
+  </Text>
+
+  <TextInput
+    autoCorrect={false}
+    onChangeText={password => setForm({...form, password})}
+    placeholder="Enter your password"
+    placeholderTextColor="#6b7280"
+    className="h-11 bg-white pl-8 pr-6 rounded-lg text-base font-medium text-gray-700 border"
+    secureTextEntry={!form.showPassword}
+    value={form.password}
+  />
+
+  <TouchableOpacity
+    onPress={togglePasswordVisibility}
+    className="absolute top-3 right-4">
+    <Icon
+      className="text-gray-500"
+      type="Ionicons"
+      name={form.showPassword ? 'eye-off-outline' : 'eye-outline'}
+      size={25}
+    />
+  </TouchableOpacity>
+</View>
