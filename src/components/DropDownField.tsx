e791e0956f36ae619a261345eacf49b98f141f19
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-13 22:51:12 -0500

    reformatted spacing in code

--- src/components/DropDownField.tsx
+++ src/components/DropDownField.tsx
@@ -19,52 +19,22 @@ const DropDownField = ({text, selectItems}: Props) => {
 
     // Probably should have all necessary properties as props, and pass them in a useState
 
-{/* <DropDownPicker className='my-2 border-2 border-black rounded-lg'
-                        // open={open}
-                        // value={value}
-                        // items={items}
-                        // setOpen={setOpen}
-                        // setValue={setValue}
-                        // setItems={setItems}
-
-                        />
-                        <Text className="font-bold my-2">Race</Text>
-                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
-                        // open={open}
-                        // value={value}
-                        // items={items}
-                        // setOpen={setOpen}
-                        // setValue={setValue}
-                        // setItems={setItems}
-
-                        />
-                        <Text className="font-bold my-2">Ethnicity</Text>
-                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
-                        // open={open}
-                        // value={value}
-                        // items={items}
-                        // setOpen={setOpen}
-                        // setValue={setValue}
-                        // setItems={setItems}
-
-                        /> */}
-
-                        const [open, setOpen] = useState(false);
-                        const [value, setValue] = useState(null);
-                        const [items, setItems] = useState(selectItems);
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState(selectItems);
 
     return (
         <View>
 
-                        <Text className="font-bold my-2 capitalize">{text}</Text>
-                        <DropDownPicker className='my-2 border-2 border-black rounded-lg'
-                        open={open}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                        dropDownDirection='TOP'
-                        />
+            <Text className="font-bold my-2 capitalize">{text}</Text>
+            <DropDownPicker className='my-2 border-2 border-black rounded-lg'
+            open={open}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            dropDownDirection='TOP'
+            />
 
 
 
