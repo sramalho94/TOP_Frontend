7cb16e03673b7329b9f4bf8f08da455c246b6d8e
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-14 14:23:55 -0500

    fixed margin bottom to none since button container was really high on the screen

--- src/screens/Onboarding/Onboard2.tsx
+++ src/screens/Onboarding/Onboard2.tsx
@@ -7,7 +7,7 @@ const Onboard2 = () => {
   return (
     <>
       <SafeAreaView className="h-screen w-screen">
-        <View className="flex-1 flex-col mt-4 mb-3">
+        <View className="flex-1 flex-col mt-4">
           {/* image and text */}
           <View className="m-5 flex-1">
             <Image className="w-full" source={NoImage}></Image>
@@ -20,7 +20,7 @@ const Onboard2 = () => {
           {/* container for dots and buttons */}
           <View className="flex-1 flex-col-reverse">
             {/* buttons */}
-            <View className="m-5">
+            <View className="mt-5 mx-5">
               <TouchableOpacity className="rounded border justify-items-center">
                 <Text className="text-center py-3 text-[22px]">Continue</Text>
               </TouchableOpacity>
