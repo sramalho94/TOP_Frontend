d95207367aeb4e7fd331b41c370cdafa9f5ad316
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-15 12:40:29 -0500

    added bottom margin on other 2 onboarding screens

--- src/screens/Onboarding/Onboard2.tsx
+++ src/screens/Onboarding/Onboard2.tsx
@@ -7,7 +7,7 @@ const Onboard2 = () => {
   return (
     <>
       <SafeAreaView className="h-screen w-screen">
-        <View className="flex-1 flex-col mt-4">
+        <View className="flex-1 flex-col mt-4 mb-5">
           {/* image and text */}
           <View className="m-5 flex-1">
             <Image className="w-full" source={NoImage}></Image>
@@ -18,7 +18,7 @@ const Onboard2 = () => {
           </View>
 
           {/* container for dots and buttons */}
-          <View className="flex-1 flex-col-reverse">
+          <View className="flex-1 flex-col-reverse mb-3">
             {/* buttons */}
             <View className="mt-5 mx-5">
               <TouchableOpacity className="rounded border justify-items-center">
