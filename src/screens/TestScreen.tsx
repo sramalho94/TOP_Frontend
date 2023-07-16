2901413de8e47ce71fa0f7098e84642f78e70360
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-16 17:09:49 -0400

    used testcreen to render screens

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -2,22 +2,16 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import Button from '../components/Button';
 import {TouchableOpacity} from 'react-native';
+import Onboard1 from './Onboarding/Onboard1';
+import Onboard2 from './Onboarding/Onboard2';
+import Onboard3 from './Onboarding/Onboard3';
+import LandingPage from './LandingPage';
+import ReportPage from './ReportPage';
+
 const TestScreen = () => {
   return (
     <View className="min-w-screen min-h-screen justify-center mx-auto">
-      <Text>TestScreen</Text>
-      <Button
-        onPress={() => console.log('fired')}
-        innerText="test button"
-        bgColor="bg-white"
-        border={true}
-        borderColor="border-4"
-        textColor="text-blue-500"
-      />
-
-      <TouchableOpacity className="border-4 border-black flex justify-center items-center w-[342] h-[52] rounded-lg bg-[#B4B4B4] mt-6 mb-8 mx-auto">
-        <Text className="text-lg font-bold">Report</Text>
-      </TouchableOpacity>
+      <ReportPage />
     </View>
   );
 };
