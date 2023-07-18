42cd3906b3500a27e2ea7dc1c74bb72f662ff874
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-18 10:14:03 -0400

    created component

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const CheckBox = (props: Props) => {
+  return (
+    <View>
+      <Text>CheckBox</Text>
+    </View>
+  );
+};
+
+export default CheckBox;
