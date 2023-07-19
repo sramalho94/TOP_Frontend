6cfb9644a94f4c95d186e5ddf4dca94b0b82c1d3
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-19 13:14:40 -0500

    set up boilerplate for homescreen

---
+++ src/screens/HomeDash.tsx
@@ -0,0 +1,25 @@
+import {
+    View,
+    Text,
+    SafeAreaView,
+    ScrollView,
+} from 'react-native';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/AntDesign'; // might need this for the avatar?
+import CircleBtn from '../components/CircleBtn';
+
+type Props = {};
+
+const HomeDash = (props: Props) => {
+
+
+    return (
+        <SafeAreaView className="h-screen w-screen">
+            <ScrollView>
+
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default HomeDash;
