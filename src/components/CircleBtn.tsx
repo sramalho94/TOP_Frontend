e7f5d138263720a0a11e2d047c2adf1222d7b6cb
kayishau <ulyssekayisha@gmail.com>
2023-07-20 18:06:15 -0400

    Added ternaries to circleBtn, still mught need refactoring

--- src/components/CircleBtn.tsx
+++ src/components/CircleBtn.tsx
@@ -12,28 +12,9 @@ type Props = {
   textSize?: string,
 };
 
-// Can use on report page screens and homescreen
+// TODO: 
+// Refactor tenaries in Touchable opacity/ make sure they work
 
-    // TODO: need to consider: do we need to add a size property for circle btn component?
-        // xsmall: w-[72] h[72]
-        // small: w-[104] h-[104]
-        // medium: w-[125] h-[125] 
-        // large: w-[182] h-[182]
-        // text color
-        // src property for image buttons??? probs
-
-    // Need to research a way to make the view/touchable opacity etc. a bg image with text on top capabilities ?
-        // style={{
-        //   backgroundImage: "require(/assets/orange-flower.png)",
-        //   height: "300px",
-        //   width: "200px",
-        //   backgroundSize: "contain",
-        //   backgroundRepeat: "no-repeat",
-        // }}
-
-        // Margin for Touchable opacity removed- conflicting with spacing on other screens - make sure to add margins to circles in views that it is imported
-
-    // Going to need to create a prop for text sizing within the circleBtn component
 
 const CircleBtn = ({textColor, textSize, img, Btnsize, text, bgColor, onPress}: Props)=> {
   return (
@@ -41,10 +22,13 @@ const CircleBtn = ({textColor, textSize, img, Btnsize, text, bgColor, onPress}: 
       <TouchableOpacity
         onPress={onPress}
         className={`${bgColor} border-2 border-black flex items-center rounded-full justify-center ${textColor} h-[${Btnsize}] w-[${Btnsize}]`}>
-         {img ?  <Image className={`bg-contain w-[${Btnsize}] h-[${Btnsize}] rounded-full`} source={img} />: <Text className={`text-center text-${textSize}`} >{text}</Text>}
+         {img && !text ?  <Image className={`border-2 border-black bg-contain w-[${Btnsize}] h-[${Btnsize}] rounded-full`} source={img} />: null }
+         {text && !img ? <Text className={`text-center text-${textSize}`} >{text}</Text> : null }
+         {img && text ? <Image className={`border-2 border-black bg-contain w-[${Btnsize}] h-[${Btnsize}] rounded-full`} source={img} /> && <Text className={`text-center text-${textSize}`} >{text}</Text> : null }
       </TouchableOpacity>
     </>
   );
 };
 
 export default CircleBtn;
+
