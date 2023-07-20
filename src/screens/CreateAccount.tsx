589bf84c1d33963aadf12bd595a8fff107beff83
Avi Patel <109503863+patel-avi@users.noreply.github.com>
2023-07-20 16:56:36 -0400

    update password criteria text in CreateAccount

--- src/screens/CreateAccount.tsx
+++ src/screens/CreateAccount.tsx
@@ -165,7 +165,7 @@ const CreateAccount = (props: Props) => {
                             <View className="flex-1 justify-center items-center bg-opacity-50">
                                 <View className="bg-white p-8 rounded-lg w-72 border-4">
                                     <Text className="text-xl font-bold mb-4">Your password must include:</Text>
-                                    <Text className="mb-2">- At least characters</Text>
+                                    <Text className="mb-2">- At least 8 characters</Text>
                                     <Text className="mb-2">- One uppercase and one lowercase letter</Text>
                                     <Text className="mb-2">- One number and one special character</Text>
                                     <Button
