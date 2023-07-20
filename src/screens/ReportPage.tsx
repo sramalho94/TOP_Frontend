21667f86dad2ef6d39a4def10fbf3659288eaa73
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-20 10:28:27 -0500

    imported circle btns on report page needed the refactored props. Also added comments to be able to view code easier

--- src/screens/ReportPage.tsx
+++ src/screens/ReportPage.tsx
@@ -17,7 +17,6 @@ import DropDownPicker from 'react-native-dropdown-picker';
 import CircleBtn from '../components/CircleBtn';
 import TopNavBar from '../components/TopNavBar';
 
-
 type Props = {};
 
 // Define the ReportPage component
@@ -52,6 +51,7 @@ const ReportPage = (props: Props) => {
   return (
     <SafeAreaView className="min-w-screen">
       <ScrollView>
+        {/* NavBar */}
         <View className="h-[90] border-b-4 border-slate-200 flex-row my-6">
           <TouchableOpacity className="mt-2 ml-4">
             <Icon name="arrowleft" size={30} color="#000" className="" />
@@ -60,28 +60,44 @@ const ReportPage = (props: Props) => {
             Report COVID-19 Test Result
           </Text>
         </View>
+
+        {/* Test Result Buttons */}
         <View className="mx-auto my-auto">
           <Text className="text-lg font-bold mx-auto">
             What were the results of your test?
           </Text>
           <View className="justify-center space-x-4 flex-row my-9">
-            <CircleBtn bgColor='bg-themeLightBlue' onPress={() => {
-              console.log("You're Clear!!")
-            }} text='Negative' size={125}/>
-            <CircleBtn text='Positive' bgColor='bg-themeLightOrange' onPress={() => {
-              console.log("You're Sick!!")
-            }} size={125}/>
+            <View className="m-2">
+              <CircleBtn
+                bgColor="bg-themeLightBlue"
+                onPress={() => console.log("You're Clear!!")}
+                text="Negative"
+                Btnsize="125"
+              />
+            </View>
+            <View className="m-2">
+              <CircleBtn
+                text="Positive"
+                bgColor="bg-themeLightOrange"
+                onPress={() => console.log("You're Sick!!")}
+                Btnsize="125"
+              />
+            </View>
           </View>
+
+          {/* Text input and dropdown fields container */}
           <View className="w-[342]">
             <TextInputField
               label="Zip Code*"
               value={zipCode}
               onChange={handleZipCodeChange}
+              placeholder="Enter your zip code"
             />
             <TextInputField
               label="Age*"
               value={age}
               onChange={handleAgeChange}
+              placeholder="Enter your age"
             />
 
             {/* TODO: will need to probs ask the UX team what the official dropdown selections are */}
@@ -89,13 +105,16 @@ const ReportPage = (props: Props) => {
             <DropDownField
               text="Gender"
               selectItems={[
-                { label: "MIGHT CHANGE BELOW SELECTION LATER", value: "MIGHT CHANGE BELOW SELECTION LATER" },
-                { label: "", value: "" },
-                { label: "Prefer not to say", value: "prefer not to say" },
+                {
+                  label: 'MIGHT CHANGE BELOW SELECTION LATER',
+                  value: 'MIGHT CHANGE BELOW SELECTION LATER',
+                },
+                {label: '', value: ''},
+                {label: 'Prefer not to say', value: 'prefer not to say'},
               ]}
               open={genderOpen}
               onOpen={() => {
-                setGenderOpen(true)
+                setGenderOpen(true);
                 setRaceOpen(false);
                 setEthnicityOpen(false);
               }}
@@ -104,19 +123,34 @@ const ReportPage = (props: Props) => {
             <DropDownField
               text="Race"
               selectItems={[
-                { label: "MIGHT CHANGE BELOW SELECTION LATER", value: "MIGHT CHANGE BELOW SELECTION LATER" },
-                { label: "American Indian or Alaska Native", value: "american indian or alaska native" },
-                { label: "Asian", value: "asian" },
-                { label: "Black or African American", value: "black or african american" },
-                { label: "Native Hawaiian or Other Pacific Islander", value: "native hawaiian or other pacific islander" },
-                { label: "Not Specified", value: "not specified" },
-                { label: "Two or More Races/Ethnicities", value: "two or more races/ethnicities" },
-                { label: "White", value: "white" },
-                { label: "Prefer not to say", value: "prefer not to say" },
+                {
+                  label: 'MIGHT CHANGE BELOW SELECTION LATER',
+                  value: 'MIGHT CHANGE BELOW SELECTION LATER',
+                },
+                {
+                  label: 'American Indian or Alaska Native',
+                  value: 'american indian or alaska native',
+                },
+                {label: 'Asian', value: 'asian'},
+                {
+                  label: 'Black or African American',
+                  value: 'black or african american',
+                },
+                {
+                  label: 'Native Hawaiian or Other Pacific Islander',
+                  value: 'native hawaiian or other pacific islander',
+                },
+                {label: 'Not Specified', value: 'not specified'},
+                {
+                  label: 'Two or More Races/Ethnicities',
+                  value: 'two or more races/ethnicities',
+                },
+                {label: 'White', value: 'white'},
+                {label: 'Prefer not to say', value: 'prefer not to say'},
               ]}
               open={raceOpen}
               onOpen={() => {
-                setGenderOpen(false)
+                setGenderOpen(false);
                 setRaceOpen(true);
                 setEthnicityOpen(false);
               }}
@@ -125,21 +159,25 @@ const ReportPage = (props: Props) => {
             <DropDownField
               text="Ethnicity"
               selectItems={[
-                { label: "MIGHT CHANGE BELOW SELECTION LATER", value: "MIGHT CHANGE BELOW SELECTION LATER" },
-                { label: "Hispanic/Latino", value: "hispanic/latino" },
-                { label: "Non-Hispanic/Latino", value: "non-hispanic/latino" },
-                { label: "Prefer not to say", value: "prefer not to say" },
+                {
+                  label: 'MIGHT CHANGE BELOW SELECTION LATER',
+                  value: 'MIGHT CHANGE BELOW SELECTION LATER',
+                },
+                {label: 'Hispanic/Latino', value: 'hispanic/latino'},
+                {label: 'Non-Hispanic/Latino', value: 'non-hispanic/latino'},
+                {label: 'Prefer not to say', value: 'prefer not to say'},
               ]}
               open={ethnicityOpen}
               onOpen={() => {
-                setGenderOpen(false)
+                setGenderOpen(false);
                 setRaceOpen(false);
                 setEthnicityOpen(true);
               }}
               setOpen={setEthnicityOpen}
             />
-
           </View>
+
+          {/* checkbox and text container */}
           <View className="flex-row justify-center my-6">
             <CheckBox
               isSelected={isCheckboxSelected}
@@ -150,6 +188,8 @@ const ReportPage = (props: Props) => {
             </Text>
           </View>
         </View>
+
+        {/* button container */}
         <View className="mb-14">
           <Button
             onPress={() => console.log('pressed')}
