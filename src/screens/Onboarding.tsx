25e04bd23d955ea0bd835de01bcbb2296d51b205
anna <anna@rcbd.org>
2023-07-20 12:37:59 -0400

    Complete task, added swipe animation when the user pressed on Continue button.

--- src/screens/Onboarding.tsx
+++ src/screens/Onboarding.tsx
@@ -1,132 +1,104 @@
-import { SafeAreaView, View, Text, Image } from 'react-native';
-import React, { useState } from 'react';
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Animated,
+  useWindowDimensions,
+} from 'react-native';
+import {ScrollView} from 'react-native';
+
 import NoImage from './../../assets/nopicture.png';
 import ProgressDots from './../components/ProgressDots';
 import Button from './../components/Button';
+
 type Props = {};
 
 const Onboarding = (props: Props) => {
-    const [currentPage, setCurrentPage] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const {width} = useWindowDimensions();
+  const scrollViewRef = useRef<ScrollView>(null);
 
-    switch (currentPage) {
-        case 1:
-            return (
-                <SafeAreaView className="h-screen w-screen">
-                    <View className="flex-1 flex-col mt-4 mb-5">
-                        {/* image and text */}
-                        <View className="m-5 flex-1">
-                            <Image className="w-full" source={NoImage}></Image>
-                            {/* TODO: maybe? change font style? Wrote font-sans earlier but it didn't change the font*/}
-                            <Text className="mx-auto py-5 font-serif text-[22px]">
-                                What the app does and the impact of data
-                            </Text>
-                        </View>
-                        {/* container for dots and buttons */}
-                        <View className="flex-1 flex-col-reverse mb-3">
-                            {/* buttons */}
-                            <View className="mt-5 mx-5">
-                                <Button
-                                    onPress={() => setCurrentPage(2)}
-                                    innerText="Continue"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={true}
-                                    borderColor="border border-gray"
-                                />
-                                <Button
-                                    onPress={() => console.log('pressed')}
-                                    innerText="Skip"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={false}
-                                    borderColor="border border-gray"
-                                />
-                            </View>
-                            {/* progress dots */}
-                            <ProgressDots page={1} />
-                        </View>
-                    </View>
-                </SafeAreaView>
-            );
-        case 2:
-            return (
-                <SafeAreaView className="h-screen w-screen">
-                    <View className="flex-1 flex-col mt-4 mb-5">
-                        {/* image and text */}
-                        <View className="m-5 flex-1">
-                            <Image className="w-full" source={NoImage}></Image>
-                            {/* TODO: maybe? change font style? Wrote font-sans earlier but it didn't change the font*/}
-                            <Text className="mx-auto py-5 text-[22px]">
-                                What the data is used for
-                            </Text>
-                        </View>
-                        {/* container for dots and buttons */}
-                        <View className="flex-1 flex-col-reverse mb-3">
-                            {/* buttons */}
-                            <View className="mt-5 mx-5">
-                                <Button
-                                    onPress={() => setCurrentPage(3)}
-                                    innerText="Continue"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={true}
-                                    borderColor="border border-gray"
-                                />
-                                <Button
-                                    onPress={() => console.log('pressed')}
-                                    innerText="Skip"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={false}
-                                    borderColor="border border-gray"
-                                />
-                            </View>
+  const pages = [
+    {
+      imageSource: NoImage,
+      title: 'What the app does and the impact of data',
+      buttonText: 'Continue',
+      pageIndicator: 1,
+      onButtonPress: () => handleSwipeAnimation(currentPage + 1),
+    },
+    {
+      imageSource: NoImage,
+      title: 'What the data is used for',
+      buttonText: 'Continue',
+      pageIndicator: 2,
+      onButtonPress: () => handleSwipeAnimation(currentPage + 1),
+    },
+    {
+      imageSource: NoImage,
+      title: 'What the data is not used for',
+      buttonText: 'Continue',
+      pageIndicator: 3,
+      onButtonPress: () => console.log('Finish button pressed'),
+    },
+  ];
 
-                            <ProgressDots page={2} />
-                        </View>
-                    </View>
-                </SafeAreaView>
-            );
-        case 3:
-            return (
-                <SafeAreaView className="h-screen w-screen">
-                    <View className="flex-1 flex-col mt-4 mb-5">
-                        {/* image and text */}
-                        <View className="m-5 flex-1">
-                            <Image className="w-full" source={NoImage}></Image>
-                            <Text className="mx-auto py-5 text-[22px]">
-                                What the data is not used for
-                            </Text>
-                        </View>
-                        {/* container for dots and buttons */}
-                        <View className="flex-1 flex-col-reverse mb-3">
-                            {/* buttons */}
-                            <View className="mt-5 mx-5">
-                                <Button
-                                    onPress={() => console.log('pressed')}
-                                    innerText="Continue"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={true}
-                                    borderColor="border border-gray"
-                                />
-                                <Button
-                                    onPress={() => console.log('pressed')}
-                                    innerText="Skip"
-                                    bgColor="bg-white"
-                                    textColor="text-black"
-                                    border={false}
-                                    borderColor="border border-gray"
-                                />
-                            </View>
-                            <ProgressDots page={3} />
-                        </View>
-                    </View>
-                </SafeAreaView>
-            );
-        default:
-            return null;
+  const swipeAnim = useRef(new Animated.Value(0)).current;
+
+  const handleSwipeAnimation = (page: number) => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({x: page * width, animated: true});
+      setCurrentPage(page);
     }
+  };
+
+  return (
+    <ScrollView
+      ref={scrollViewRef}
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      scrollEnabled={false} // Disable manual scrolling
+    >
+      {pages.map((page, index) => (
+        <SafeAreaView key={index} className="h-screen w-screen">
+          <View className="flex-1 flex-col mt-4 mb-5">
+            <View className="m-5 flex-1">
+              <Image className="w-full" source={page.imageSource}></Image>
+              <Text className="mx-auto py-5 font-serif text-[22px]">
+                {page.title}
+              </Text>
+            </View>
+            <View className="flex-1 flex-col-reverse mb-3">
+              <View className="mt-5 mx-5">
+                <Button
+                  onPress={page.onButtonPress} // Updated this line
+                  innerText={page.buttonText}
+                  bgColor="bg-white"
+                  textColor="text-black"
+                  border={true}
+                  borderColor="border border-gray"
+                />
+                {currentPage !== pages.length && (
+                  <Button
+                    onPress={() => console.log('Skip button pressed')}
+                    innerText="Skip"
+                    bgColor="bg-white"
+                    textColor="text-black"
+                    border={false}
+                    borderColor="border border-gray"
+                  />
+                )}
+              </View>
+              <ProgressDots page={page.pageIndicator} />
+            </View>
+          </View>
+        </SafeAreaView>
+      ))}
+    </ScrollView>
+  );
 };
 
 export default Onboarding;
