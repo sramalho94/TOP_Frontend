c74c19ed1f7b9b748f5b57c1887a945f1eb3c4f5
kayishau <ulyssekayisha@gmail.com>
2023-07-24 18:17:34 -0400

    formatting

--- src/components/CircleBtn.tsx
+++ src/components/CircleBtn.tsx
@@ -1,6 +1,6 @@
 import {View, Text, TouchableOpacity, Animated} from 'react-native';
 import React, {useRef, useEffect, useState} from 'react';
-import { Image } from 'react-native';
+import {Image} from 'react-native';
 
 type Props = {
   text?: string;
@@ -10,26 +10,47 @@ type Props = {
   Btnheight: string;
   textColor?: string;
   img?: any;
-  textSize?: string,
+  textSize?: string;
 };
 
-// TODO: 
+// TODO:
 // Refactor tenaries in Touchable opacity/ make sure they work
 
-
-const CircleBtn = ({textColor, textSize, img, Btnwidth, Btnheight, text, bgColor, onPress}: Props)=> {
+const CircleBtn = ({
+  textColor,
+  textSize,
+  img,
+  Btnwidth,
+  Btnheight,
+  text,
+  bgColor,
+  onPress,
+}: Props) => {
   return (
     <>
       <TouchableOpacity
         onPress={onPress}
         className={`${bgColor} border-2 border-black flex items-center rounded-full justify-center ${textColor} ${Btnheight} ${Btnwidth}`}>
-         {img && !text ?  <Image className={`bg-contain ${Btnheight} ${Btnwidth} rounded-full`} source={img} />: null }
-         {text && !img ? <Text className={`text-center text-${textSize}`} >{text}</Text> : null }
-         {img && text ? <Image className={`bg-contain ${Btnheight} ${Btnwidth} rounded-full`} source={img} /> && <Text className={`text-center text-${textSize}`} >{text}</Text> : null }
+        {img && !text ? (
+          <Image
+            className={`bg-contain ${Btnheight} ${Btnwidth} rounded-full`}
+            source={img}
+          />
+        ) : null}
+        {text && !img ? (
+          <Text className={`text-center text-${textSize}`}>{text}</Text>
+        ) : null}
+        {img && text
+          ? (
+              <Image
+                className={`bg-contain ${Btnheight} ${Btnwidth} rounded-full`}
+                source={img}
+              />
+            ) && <Text className={`text-center text-${textSize}`}>{text}</Text>
+          : null}
       </TouchableOpacity>
     </>
   );
 };
 
 export default CircleBtn;
-
