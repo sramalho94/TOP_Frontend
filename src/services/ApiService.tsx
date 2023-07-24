3cbc14e559e835d461b0596f8d0888fa18bb78b2
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-07-24 16:08:09 -0500

    added axios as dependency, functions in class added

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,36 +1,70 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://10.0.2.2:3001/api'; // Replace with your actual API base URL
 
 export default class ApiService {
 
-//     static getWeather = async (cityName: string, apiUnit: string): Promise<Response> => {
+  // NOTE: These are NOT tested yet! Need to fix "Create Account" screen in order to test the first route: "register".
 
-//         return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}
-//         &appid=${API_TOKEN}&units=${apiUnit}`)
-//     }
+    // Authentication API calls
+  static async login(credentials) {
+    return axios.post(`${BASE_URL}/auth/login`, credentials);
+  }
+
+  static async register(userData) {
+    return axios.post(`${BASE_URL}/auth/register`, userData);
+  }
+
+  static async checkSession() {
+    // Add any necessary authentication headers, such as the JWT token
+    return axios.get(`${BASE_URL}/auth/check-session`);
+  }
+
+  static async updatePassword(username, oldPassword, newPassword) {
+    return axios.put(`${BASE_URL}/auth/update-password`, {
+      username,
+      oldPassword,
+      newPassword,
+    });
+  }
+    // User-related API calls
+  static async getAllUsers() {
+    return axios.get(`${BASE_URL}/users`);
+  }
+
+  static async getUserById(id) {
+    return axios.get(`${BASE_URL}/users/${id}`);
+  }
+
+  static async updateUser(id, userData) {
+    return axios.put(`${BASE_URL}/users/${id}`, userData);
+  }
+
+  static async deleteUser(id) {
+    return axios.delete(`${BASE_URL}/users/${id}`);
+  }
+
+  // Test-related API calls
+  static async getAllTests() {
+    return axios.get(`${BASE_URL}/tests`);
+  }
+
+  static async getTestById(id) {
+    return axios.get(`${BASE_URL}/tests/${id}`);
+  }
+
+  static async createTest(testData) {
+    return axios.post(`${BASE_URL}/tests`, testData);
+  }
+
+  static async updateTest(id, testData) {
+    return axios.put(`${BASE_URL}/tests/${id}`, testData);
+  }
+
+  static async deleteTest(id) {
+    return axios.delete(`${BASE_URL}/tests/${id}`);
+  }
+
   
-//     static getDBWeather = () => {
-
-//         return axios.get(`http://10.0.2.2:3000/api/v1/weather`)
-           
-//    }
-
-//     static handleAddCity = (name: string) => {
-
-//         return axios.post("http://10.0.2.2:3000/api/v1/weather", name)
-
-//     }
-
-
-//     static handleDelete = (id: number) => {
-//         return axios.delete("http://10.0.2.2:3000/api/v1/weather/" + id)
-//     }
-
-    static REPLACENAME = async (): Promise<Response> => {
-
-        return await fetch() //api call
-
-    }
-
-
 }
+
