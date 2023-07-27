bb61357056a2975fe0ae2cefb73a38f5213947bc
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-27 17:12:59 -0400

    integrated AuthContext and logic for which screen will render

--- App.tsx
+++ App.tsx
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import LandingPage from './src/screens/LandingPage';
 import ReportPage from './src/screens/ReportPage';
 import AccountReportPage from './src/screens/AccountReportPage';
@@ -15,32 +8,86 @@ import CreateAccount from './src/screens/CreateAccount';
 import ConsentPage from './src/screens/ConsentPage';
 import Onboarding from './src/screens/Onboarding';
 import HomeDash from './src/screens/HomeDash';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import DropDownField from './src/components/DropDownField';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {AuthProvider, useAuth} from './app/context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type RootStackParamList = {
-  ThankYouScreen: { logIn: boolean };
+  Onboarding: {navigation: any};
+  LandingPage: undefined;
+  SignInPage: undefined;
+  CreateAccount: undefined;
+  ReportPage: undefined;
+  ConsentPage: undefined;
+  ThankYouScreen: {logIn: boolean};
+  AccountReportPage: undefined;
+  HomeDash: undefined;
 };
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): JSX.Element {
-  return (
+  const navigationRef =
+    useRef<NavigationContainerRef<RootStackParamList>>(null);
+  const [firstTime, setFirstTime] = useState(true);
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const isFirstTime = await AsyncStorage.getItem('first_time');
+        if (isFirstTime !== null) {
+          setFirstTime(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <AuthProvider>
+      <AppContent navigationRef={navigationRef} firstTime={firstTime} />
+    </AuthProvider>
+  );
+}
+
+function AppContent({
+  navigationRef,
+  firstTime,
+}: {
+  navigationRef: React.RefObject<NavigationContainerRef<RootStackParamList>>;
+  firstTime: boolean;
+}) {
+  const {authState} = useAuth();
+
+  useEffect(() => {
+    if (firstTime) {
+      navigationRef.current?.navigate('Onboarding');
+    } else if (authState?.authenticated) {
+      navigationRef.current?.navigate('HomeDash');
+    } else {
+      navigationRef.current?.navigate('LandingPage');
+    }
+  }, [authState, firstTime, navigationRef]);
+
+  return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <Stack.Navigator>
           <Stack.Screen
             name="Onboarding"
             component={Onboarding}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
           <Stack.Screen
             name="LandingPage"
             component={LandingPage}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
           <Stack.Screen name="SignInPage" component={SignInPage} />
           <Stack.Screen name="CreateAccount" component={CreateAccount} />
@@ -49,7 +96,7 @@ function App(): JSX.Element {
           <Stack.Screen
             name="ThankYouScreen"
             component={ThankYouScreen}
-            options={{ headerShown: false }}
+            options={{headerShown: false}}
           />
           <Stack.Screen
             name="AccountReportPage"
@@ -59,7 +106,6 @@ function App(): JSX.Element {
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
-
   );
 }
 
