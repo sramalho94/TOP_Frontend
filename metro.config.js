cb2bbeaece03f8d08e752235702208f5d8571a6f
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-08-09 14:56:22 -0500

    reverted changes to metro config, also downloaded react native webview to get rid of error message

--- metro.config.js
+++ metro.config.js
@@ -8,32 +8,4 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config), (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();;
-
-// module.exports = (async () => {
-//     const {
-//       resolver: { sourceExts, assetExts },
-//     } = await getDefaultConfig();
-//     return {
-//       transformer: {
-//         babelTransformerPath: require.resolve('react-native-svg-transformer'),
-//       },
-//       resolver: {
-//         assetExts: assetExts.filter(ext => ext !== 'svg'),
-//         sourceExts: [...sourceExts, 'svg'],
-//       },
-//     };
-//   })();
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
