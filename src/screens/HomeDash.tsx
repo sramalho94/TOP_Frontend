b90c4e3d7343006e5e59c51f370aedee5f8f165b
kayishau <ulyssekayisha@gmail.com>
2023-08-12 12:16:45 -0400

    edits

--- src/screens/HomeDash.tsx
+++ src/screens/HomeDash.tsx
@@ -4,14 +4,15 @@ import CircleBtn from '../components/CircleBtn';
 import {useAuth} from '../context/AuthContext';
 import FlowerImg from '../../assets/orange-flower.png';
 import CTL from '../../assets/communityTestLab.png';
+import Avatar from '../../assets/Avatar.png';
 
 const HomeDash: React.FC<{navigation: any}> = ({navigation}) => {
-  const {onLogout} = useAuth();
+  const {onLogout, usernameVal} = useAuth();
   return (
     <SafeAreaView className="h-screen w-screen">
       <ScrollView>
         {/* View for whole screen */}
-        <View className="flex flex-col w-full min-h-screen bg-slate-500">
+        <View className="flex flex-col w-full min-h-screen">
           {/* View for avatar, text and report test */}
           <View className="flex w-full justify-center items-center">
             {/* View for avatar*/}
@@ -21,10 +22,10 @@ const HomeDash: React.FC<{navigation: any}> = ({navigation}) => {
             className="w-full h-[200] mb-2 bg-cover bg-center "
             source={CTL}
           />
-            <View className="self-start mb-2 pl-5">
+            <View className="flex flex-row space-between mb-2 pl-5">
               <CircleBtn
-                img={FlowerImg}
-                bgColor="bg-themeLightOrange"
+                img={Avatar}
+                borderColor="border-themeWhite"
                 onPress={() => {
                   if (onLogout) {
                     onLogout();
@@ -32,13 +33,11 @@ const HomeDash: React.FC<{navigation: any}> = ({navigation}) => {
                     console.error('onLogout function is not provided');
                   }
                 }}
-                Btnwidth="w-14"
-                Btnheight="h-14"
+                Btnwidth="w-20"
+                Btnheight="h-20"
               />
+              <Text>Welcome back, {usernameVal}!</Text>
             </View>
-            <Text className="text-3xl font-bold mb-2 text-center">
-              Community {'\n'}Test Lab
-            </Text>
             {/* need giant circle btn, need to refactor circle btn props? */}
             <CircleBtn
               text={`Report\nTest`}
@@ -46,8 +45,8 @@ const HomeDash: React.FC<{navigation: any}> = ({navigation}) => {
               onPress={() => {
                 navigation.navigate('AccountReportPage');
               }}
-              Btnwidth="w-60"
-              Btnheight="h-60"
+              Btnwidth="w-48"
+              Btnheight="h-48"
               textSize="3xl"
             />
           </View>
@@ -57,34 +56,32 @@ const HomeDash: React.FC<{navigation: any}> = ({navigation}) => {
             <View className="-mt-5">
               <CircleBtn
                 img={FlowerImg}
+                text="Privacy"
                 onPress={() => {
                   console.log("You're Sick!!");
                 }}
                 Btnwidth="w-28"
                 Btnheight="h-28"
               />
-              <Text className="text-center p-3 text-black text-base">
-                Privacy
-              </Text>
             </View>
 
             <View className="mt-16">
               <CircleBtn
+                text='Dashboard'
                 img={FlowerImg}
                 onPress={() => navigation.navigate('DataDashboard')}
                 Btnwidth="w-28"
                 Btnheight="h-28"
               />
-              <Text className="text-center p-3 text-black text-base">
-                Dashboard
-              </Text>
             </View>
 
             <View className="mt-1">
-              <CircleBtn img={FlowerImg} Btnwidth="w-28" Btnheight="h-28" />
-              <Text className="text-center p-3 text-black text-base">
-                Exchange
-              </Text>
+              <CircleBtn 
+              text='Exchange'
+              img={FlowerImg} 
+              Btnwidth="w-28" 
+              Btnheight="h-28" />
+            
             </View>
           </View>
         </View>
