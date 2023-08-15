a3d444b1c8bf535fbec22efeb56ac69e020f8419
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-15 10:44:45 -0400

    removed unecessary comments

--- src/components/DashButton.tsx
+++ src/components/DashButton.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 type Props = {
   text?: string;
   bgColor?: string;
-  updateForm?: (field: string, value: string | boolean) => void; // Make this optional
-  onPress?: () => void; // Add custom onPress prop
+  updateForm?: (field: string, value: string | boolean) => void;
+  onPress?: () => void;
   Btnwidth: string;
   Btnheight: string;
   textColor?: string;
   img?: any;
   textSize?: string;
-  value?: boolean; // Make this optional
+  value?: boolean;
   accessLabel: string;
   accessHint: string;
 };
