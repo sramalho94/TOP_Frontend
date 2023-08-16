ecef7e4b8672997f84066f81684d810561e1fd89
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-16 10:50:14 -0400

    removed unecessary comment

--- src/components/ProgressDots.tsx
+++ src/components/ProgressDots.tsx
@@ -6,24 +6,23 @@ type Props = {
 };
 
 const ProgressDots = ({page}: Props) => {
-  // TODO: make it more dynamic! Probably need to use map? Not necessary for this project, but good if we end up having more than three dots in the future
-
   useEffect(() => {}, [page]);
 
   return (
     <>
-      <View className="flex-row justify-center" accessibilityLabel="Progress Markers" accessibilityHint="Shows progress through pages on swipe or button press">
+      <View
+        className="flex-row justify-center"
+        accessibilityLabel="Progress Markers"
+        accessibilityHint="Shows progress through pages on swipe or button press">
         <View
           className={`rounded-lg ${
             page === 1 ? 'bg-themeBlue' : 'bg-themeLightBlue'
           } w-3 h-3 m-1`}
-          accessibilityLabel="Progress Marker 1"
-          ></View>
+          accessibilityLabel="Progress Marker 1"></View>
         <View
           className={`rounded-lg ${
             page === 2 ? 'bg-themeBlue' : 'bg-themeLightBlue'
-          } w-3 h-3 m-1`}
-          ></View>
+          } w-3 h-3 m-1`}></View>
         <View
           className={`rounded-lg ${
             page === 3 ? 'bg-themeBlue' : 'bg-themeLightBlue'
