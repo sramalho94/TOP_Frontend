9f2abd14ea6c55050689bf8cd41ac75bc9076015
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-16 10:51:53 -0400

    removed unecessary comment

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -1,8 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/AntDesign';
 import ProgressDots from './ProgressDots';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 type Props = {
   textSize: string;
@@ -11,9 +11,6 @@ type Props = {
   haveProgress: boolean;
   page?: number;
 };
-
-// Add a page prop for the progressDots component.
-// haveProgress prop is a little bit redundant because you can use page to tell whether its haveProgress is true or false but I'll just leave it for now for whoever take over topNavBar to understand/implement this component more easily.
 
 const TopNavBar = ({
   textSize,
@@ -35,9 +32,7 @@ const TopNavBar = ({
           onPress={() => navigation.goBack()}
           accessible={true}
           accessibilityLabel="Back"
-          accessibilityHint="Navigates to the previous screen"
-          >
-          
+          accessibilityHint="Navigates to the previous screen">
           <Icon name="arrowleft" size={30} color="#000" className="" />
         </TouchableOpacity>
         <Text
@@ -46,10 +41,7 @@ const TopNavBar = ({
         </Text>
       </View>
       <View className="shadow shadow-black ">
-        <View className="border-gray-200 border-b-2  pb-4">
-          {/* <View className="shadow shadow-red-500/40 md:shadow-red-500/40"> */}
-          {/* {haveProgress ? page ? <ProgressDots page={page} /> : null : null} */}
-        </View>
+        <View className="border-gray-200 border-b-2  pb-4"></View>
       </View>
     </View>
   );
